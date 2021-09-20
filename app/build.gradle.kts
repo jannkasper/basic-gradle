71f246ce9c1eef30fb6489c30ca88aefc07db93a
jannkasper <jann.kasper@gmail.com>
2021-09-20 07:19:53 +0200

    Add project-a & project-b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,10 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
+
+    implementation(project(":project-a"))
+    implementation(project(":project-b"))
+
 }
 
 application {
